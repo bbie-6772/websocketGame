4c62db9b94405edae916b69a57f20679b888db4b
조용필 <78397393+bbie-6772@users.noreply.github.com>
2024-12-19 19:59:16 +0900

    Fix: 삭제기능 제거
    기존 닉네임과 uuid를 접속이 끝나면 삭제하도록 하는 로직을 수정

--- src/handlers/helper.js
+++ src/handlers/helper.js
@@ -1,24 +1,26 @@
 import { CLIENT_VERSION } from "../constant.js"
-import { getUser, removeUser } from "../models/user.model.js"
+import { getUser } from "../models/user.model.js"
 import { removeStage } from "../models/stage.model.js"
 import handlerMappings from "./handler.Mapping.js"
+import { getRanking, loadRanking } from "../models/ranking.model.js"
 
-export const handleDisconnect = (socket, uuid) => {
-    removeUser(socket.id)
-    removeStage(uuid)
-    console.log('User disconnected: ',socket.id)
-    console.log('Current users: ',getUser())
-}
-
-export const handleConnection = (socket, uuid) => {
-    console.log(`New user connected: ${uuid} with socket Id ${socket.id}` );
+export const handleConnection = (socket, userInfo) => {
+    loadRanking()
+    console.log(`New user connected: ${userInfo.uuid} with socket Id ${socket.id}` );
     console.log('Current users: ', getUser())
 
     //유저와 연결되면 uuid를 메세지로 전달
-    socket.emit('connection', {uuid})
+    socket.emit('connection', userInfo )
+}
+
+export const handleDisconnect = (socket, uuid) => {
+    removeStage(uuid)
+    console.log('User disconnected: ', socket.id)
+    console.log('Current users: ', getUser())
 }
 
 export const handlerEvent = (io, socket, data) => {
+    loadRanking()
     //클라이언트 버전 확인
     if (!CLIENT_VERSION.includes(data.clientVersion)) {
         socket.emit('response', { 
@@ -37,11 +39,12 @@ export const handlerEvent = (io, socket, data) => {
         return;
     }
 
-    const response = handler(data.userId, data.payload);
+    const response = handler(data.userId, data.payload)
 
     // 디버깅용 확인
     if (response.status !== "success") console.log(response)
 
+    io.emit('rank', getRanking())
     // 서버 전 유저에게 알림
     if (response.broadcast) {
         io.emit('response', response);
