aabe59c27d28ef32c09c16e7dd305ba33465d97a
조용필 <78397393+bbie-6772@users.noreply.github.com>
2024-12-19 19:57:36 +0900

    Feat: 닉네임 기능 추가
    닉네임 기능을 추가하기 위해 닉네임을 찾아 보내주는 추가 로직을 설정

--- src/handlers/chat.handler.js
+++ src/handlers/chat.handler.js
@@ -1,4 +1,7 @@
+import { getUser } from "../models/user.model.js";
+
 // 맵핑이 될 함수
 export const handleChat = (uuid, payload) => {
-    return { status: "success", id: uuid, msg: payload, broadcast: true };
+    const user = getUser().find((e) => e.uuid === uuid)
+    return { status: "success", id: uuid, nickname: user.nickname ,msg: payload, broadcast: true };
 };
