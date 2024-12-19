35c6f39ba6d5a56650eba6ce65e3bb9ca8a8b9e4
조용필 <78397393+bbie-6772@users.noreply.github.com>
2024-12-19 19:59:57 +0900

    Feat: 유저 정보 읽기 기능 추가
    유저 정보를 읽어 없으면 새로 만들고, 있으면 그 값을 다시 클라이언트에게 보내주도록 수정

--- src/handlers/register.handler.js
+++ src/handlers/register.handler.js
@@ -1,18 +1,40 @@
-import { addUser} from "../models/user.model.js"
+import { addUser, getUser, setUserSocket } from "../models/user.model.js"
 //uuid 생성 버전4 
 import { v4 as uuidv4 } from "uuid"
 import { handleConnection, handleDisconnect, handlerEvent } from "./helper.js";
+import { CLIENT_VERSION } from "../constant.js"
+import { getRanking } from "../models/ranking.model.js";
 
 
 const registerHandler = (io) => {
     // 모든 유저가 '연결' 시 콜백함수 실행
     io.on('connection', (socket) => {
-        //uuid 생성
-        const userUUID = uuidv4();
-        //유저 추가
-        addUser({ uuid: userUUID, socketId: socket.id});
+        // 첫 접속 값 가져오기
+        const information = socket.handshake.query
+        // 접속 시 클라이언트 버전 확인
+        if (!CLIENT_VERSION.includes(information.clientVersion)) socket.emit('response', { status: "fail" })
 
-        handleConnection(socket, userUUID)
+        // 접속 시 userId의 uuid 검증 후 uuid 생성
+        const UUID = /^[{(]?([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})[)}]?$/  
+
+        const userUUID = UUID.test(information.userId) ? information.userId : uuidv4()
+        const nickname = information?.nickname || null
+
+        // 유저 확인
+        let userInfo = getUser().find((e) => e.uuid === userUUID)
+        // 서버에 유저가 없을 경우
+        if (!userInfo) {
+            userInfo = { uuid: userUUID, socketId: socket.id, nickname ,highScore: 0, itemScore: 0 }
+            //유저 생성
+            addUser(userInfo);
+        } else {
+            // 유저 수정
+            setUserSocket(userUUID, socket.id, nickname)
+        }
+
+        // 만든 유저 정보를 클라이언트로 전달
+        handleConnection(socket, userInfo)
+        io.emit('rank', getRanking())
 
         // '이벤트' 발생 시 맵핑 실행
         socket.on('event', (data) => handlerEvent(io, socket, data));
