31dbfe62601dcbaefbd71db5c9a2dc85d8fac028
조용필 <78397393+bbie-6772@users.noreply.github.com>
2024-12-19 20:00:18 +0900

    Feat: 랭킹시스템을 저장할 model

---
+++ src/models/ranking.model.js
@@ -0,0 +1,15 @@
+import { getUser } from "./user.model.js";
+
+// 서버에 메모리형식으로 접속되어있는 ranking 저장
+let rankings = [];
+
+export const loadRanking = () => {
+    const rank = getUser().sort((a, b) => a.highScore - b.highScore).map((e) => {
+        return [e.nickname, e.highScore]
+    }).slice(0, 9)
+    rankings = rank
+}
+
+export const getRanking = () => {
+    return rankings
+}
